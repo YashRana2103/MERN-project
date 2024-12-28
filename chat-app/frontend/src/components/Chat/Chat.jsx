904853690a657f9450508f2047356eeae224e733
Yash <yashrana21032006@gmail.com>
2024-12-28 13:05:20 +0530

    .

---
+++ chat-app/frontend/src/components/Chat/Chat.jsx
@@ -0,0 +1,9 @@
+// src/components/Chat/Chat.jsx
+import React from "react";
+import "./Chat.css";
+
+const Chat = () => {
+  return <div className="chat">{/* Chat UI */}</div>;
+};
+
+export default Chat;

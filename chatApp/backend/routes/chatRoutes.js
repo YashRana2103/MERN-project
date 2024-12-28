7bdb0d6f31579fe252a9410e7c3c959db26db6e4
Yash <yashrana21032006@gmail.com>
2024-12-28 16:19:40 +0530

    .

--- chatApp/backend/routes/chatRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getMessages, saveMessage } = require("../controllers/chatController");
-
-const router = express.Router();
-
-router.get("/messages", getMessages);
-router.post("/messages", saveMessage);
-
-module.exports = router;
